8413b4dec5a3c7114f2111ed0cca50e54b79deff
hding49 <hding49@uwo.ca>
2020-07-31 21:11:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
-let port = 8080;
+let port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
